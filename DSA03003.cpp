f1608877b8be0e2f55278710b737e5b22e177499
Nguyen Truong Giang <GiangNT.B23CN259@stu.ptit.edu.vn>
2025-02-17 21:41:21 +0700

    17/2

---
+++ DSA03003.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mod = 1e9 + 7;
+
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        int a[n];
+        for (auto &x : a){
+            cin >> x;
+        }
+        sort (a, a + n);
+        long long sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += ((1ll * a[i] * i) % mod);
+            sum %= mod;
+        }
+        cout << sum << endl;
+    }
+}
