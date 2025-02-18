ce993837c40134bc2b7e9a081800d47e0ec9c7a3
Nguyen Truong Giang <GiangNT.B23CN259@stu.ptit.edu.vn>
2025-02-18 09:49:55 +0700

    18/2

---
+++ PTSNT.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+using str = string;
+using ll = long long;
+int mod = 1e9 + 7;
+
+void ans (int n){
+    int tmp = n;
+    for (int i = 2; i <= sqrt(tmp); i++){
+        if (n % i == 0){
+            int cnt = 0;
+            while (n % i == 0){
+                ++cnt;
+                n /= i;
+            }
+            cout << i << "(" << cnt << ")" << " ";
+        }
+    }
+    if (n > 1){
+        cout << n << "(1)";
+    }
+    cout << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    int tc = 0;
+    while (t--){
+        ++tc;
+        int n;
+        cin >> n;  
+        cout << "Test " << tc << ": ";
+        ans(n);
+    }
+}
