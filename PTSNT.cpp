b2a17244aefeb73b2185e9b5b65072bc55768b73
Nguyen Truong Giang <GiangNT.B23CN259@stu.ptit.edu.vn>
2025-02-18 10:02:21 +0700

    18/2

--- PTSNT.cpp
+++ PTSNT.cpp
@@ -5,8 +5,7 @@ using ll = long long;
 int mod = 1e9 + 7;
 
 void ans (int n){
-    int tmp = n;
-    for (int i = 2; i <= sqrt(tmp); i++){
+    for (int i = 2; i <= sqrt(n); i++){
         if (n % i == 0){
             int cnt = 0;
             while (n % i == 0){
